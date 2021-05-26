ee932d19556991ba6ec2e5170d4a84b99361fcd9
AlanSoto31 <soto.alan@hotmail.com>
2021-05-26 10:47:51 -0500

    Add calculate function in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
+import calculate from '../logic/calculate';
+
+const data1 = {
+  total: '0.5',
+  next: '5.5',
+  operation: null,
+};
 
 function App() {
+  const dataResult = calculate(data1, '+');
   return (
     <>
       <ButtonPanel />
-      <Display result="100" />
+      <Display result={dataResult.total} />
     </>
   );
 }
