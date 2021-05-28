a6fc0dd25bc934addc6e99addb6f56f7eccb45bf
AlanSoto31 <soto.alan@hotmail.com>
2021-05-28 15:28:44 -0500

    Refactor App class component to a functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const App = () => {
+  const [values, setValues] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  handleClick(buttonName) {
-    const op = calculate(this.state, buttonName);
-    this.setState(() => ({
+  const handleClick = (buttonName) => {
+    const op = calculate(values, buttonName);
+    setValues({
       total: op.total,
       next: op.next,
       operation: op.operation,
-    }));
-  }
+    });
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <>
-        <ButtonPanel clickHandler1={(buttonName) => this.handleClick(buttonName)} />
-        <Display result={operation ? next : total} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <ButtonPanel clickHandler1={(buttonName) => handleClick(buttonName)} />
+      <Display result={values.operation ? values.next : values.total} />
+    </>
+  );
+};
 
 export default App;
